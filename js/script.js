6c80071dc60d0e797efff94e9758a59592f37258
Abdul Samad <asamad384@gmail.com>
2021-08-26 02:04:13 +0500

    Update link

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ $(document).ready(function(){
   $('button').click(function(){
    const name = $('#nameid').val();
    $('.imgchange').attr('src',`https://joeschmoe.io/api/v1/${name}`);
-   //const b = $('.imgchange');
-   //console.log(b);
 
    if(!name) {
    	$('.imgchange').attr('src','img/placholder.jpg');
@@ -22,6 +20,6 @@ $(document).ready(function(){
 //Developed by Abdul
 $(document).ready(function(){
     //Developer credit text
-    $("#d-by").append(' <a href="https://www.linkedin.com/in/abdulsamad0fficial/">Developed by Abdul</a>');
+    $("#d-by").append(' <a href="https://getabdulsamad.com/">Developed by Abdul</a>');
     $("#d-by a").attr("target", "_blank");
 });
